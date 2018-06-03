7a87b3371332317dfe456edb0b9f305daa421427
jonathanerik <39649732+jonathanerik@users.noreply.github.com>
2018-06-03 11:19:27 +0800

    Update and rename dbconnect.php to dbconnect.inc

--- dbconnect.php
+++
@@ -1,5 +0,0 @@
-<?php
-$con=mysqli_init(); mysqli_ssl_set($con, NULL, NULL, null, NULL, NULL);
- mysqli_real_connect($con, "oprosys-mysqldbserver.mysql.database.azure.com", "akashil@oprosys-mysqldbserver", "P@ssw0rd", "oprosys","3306");
- ?>
-
